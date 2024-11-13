8b8117c3f7b4e644cc30a01f57bf18e24c5ab24a
NicolasAdvise <nicolas@advise.fr>
2024-11-13 13:52:03 +0100

    Excercice 2 : Initialisation et configuration du projet

---
+++ dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function welcomeMessage() {
+    return "Bienvenue dans le projet d'apprentissage de Typescript";
+}
+console.log(welcomeMessage());
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+function welcomeMessage(): string {
+    return "Bienvenue dans le projet d'apprentissage de Typescript"
+}
+
+console.log(welcomeMessage());
