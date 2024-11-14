cc528456b3b4cf439db5d150ae780f7a7bc0c259
nicolasAdvise <nicolas@advise.fr>
2024-11-14 15:59:23 +0100

    Excercice : Définir des types avec des unions et intersections.

--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -1,7 +1,30 @@
-export interface Vehicle {
-    model: string;
-    make: string;
-    year: number;
+type Car = {
+    numberOfDoors: number
 }
 
+type Truck = {
+    cargoCapacity: number
+}
 
+type VehicleType = Car | Truck;
+
+function isCar(vehicle: VehicleType): vehicle is Car {
+    return (vehicle as Car).numberOfDoors !== undefined;
+}
+
+function isTruck(vehicle: VehicleType): vehicle is Truck {
+    return (vehicle as Truck).cargoCapacity !== undefined;
+}
+function getVehicleDescription(vehicle : VehicleType) : string {
+    if (isCar(vehicle)) {
+        return "Cette voiture possède "+vehicle.numberOfDoors+"."
+    }else{
+        return "Ce camion a une capacité de chargement de "+vehicle.cargoCapacity+"kg."
+    }
+}
+
+const myCar: Car = { numberOfDoors: 4 };
+const myTruck: Truck = { cargoCapacity: 200 };
+
+console.log(getVehicleDescription(myCar))
+console.log(getVehicleDescription(myTruck))
