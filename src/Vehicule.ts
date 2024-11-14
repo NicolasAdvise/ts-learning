1b58e5b83b37a348c8a6690bb5e21473b1cf7a67
nicolasAdvise <nicolas@advise.fr>
2024-11-14 14:31:39 +0100

    Excercice : Utiliser des types avancés pour plus de flexibilité

--- src/Vehicule.ts
+++ src/Vehicule.ts
@@ -1,13 +1,30 @@
-function calculateRentalCost(duration: number, cost: number): number {
+function calculateRentalCost(duration: unknown, cost: unknown): number | string {
+    if (typeof duration !== 'number' || typeof cost !== 'number') {
+        return "Erreur : Les paramètres doivent être de type nombre.";
+    }
     return duration * cost;
 }
 
 for (let i = 1; i <= 8; i++) {
-    let duration: number = Math.floor(Math.random() * 20) + 1;
-    let cost: number = Math.floor(Math.random() * 100) + 1;
+    let duration: any;
+    let cost: any;
+
+    if (i === 3) {
+        duration = "something went wrong";
+        cost = Math.floor(Math.random() * 100) + 1;
+    } else if (i === 5) {
+        duration = Math.floor(Math.random() * 20) + 1;
+        cost = "something went wrong";
+    } else {
+        duration = Math.floor(Math.random() * 20) + 1;
+        cost = Math.floor(Math.random() * 100) + 1;
+    }
+
     let total = calculateRentalCost(duration, cost);
 
     console.log(
-        `Pour une location de ${duration} jours au prix journalier de ${cost}€, le prix total s'élève à ${total}€`
-    );
+        typeof total === "number"
+            ? `Pour une location de ${duration} jours au prix journalier de ${cost}€, le prix total s'élève à ${total}€`
+            : total
+    )
 }
